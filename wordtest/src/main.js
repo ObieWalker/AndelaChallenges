d9470fccb63ae3c41ce2a4cbf22372772e36ebb2
ObieWalker <obinnawalker@gmail.com>
2017-11-20 18:54:45 +0100

    word counter test

---
+++ wordtest/src/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+
+words : (arg) => {
+
+  if(typeof arg === 'string' && arg !== 'null'){
+    arg = arg.toLowerCase();
+    arg = arg.replace(/\s{2,}/g, ' ')//removes unneccessary white space
+    arg = arg.split(" ")//splits sentence into an array
+      let obj = {}
+      arg.forEach(function(key){
+         
+        if (!(key in obj)){
+           var value = 0
+           obj[key]=value+1
+          
+        }else{
+          obj[key]=parseInt(obj[key])+1
+        }
+      })
+      
+      return obj
+      }
+      else{
+        return 'please input a string';
+    }
+  }
+}
---
+++ wordtest/tests/test.js
@@ -0,0 +1,36 @@
+const assert  = require('chai').assert;
+const myApp = require('../src/main.js');
+
+describe("Words", function(){
+
+	describe("handle valid input", function(){
+		it("should return { i: 2, go: 3, dey: 2, come: 1, soon: 1 } as an input for 'I go dey come i go dey go soon'", function(){
+     		assert.notStrictEqual(myApp.words("I go dey come i go dey go soon"), { i: 2, go: 3, dey: 2, come: 1, soon: 1 });
+		});
+	})
+
+	describe("handle invalid input", function(){
+		it("should return 'please input a string' for argument 1234", function(){
+			assert.equal(myApp.words(1234), 'please input a string');
+		});
+	})
+
+	describe("handle null input", function(){
+		it("should return 'please input a string' for a null input", function(){
+			assert.equal(myApp.words(), 'please input a string');
+		});
+	})
+
+	describe("handle same word with different case", function(){
+		it("should return { there: 2 } for input of 'There there'", function(){
+			assert.notStrictEqual(myApp.words("There there"), { there: 2 });
+		});
+	})
+
+	describe("handle excess white space", function (){
+		it("should return for input of { one: 2, there: 1, was: 2 }'one there was was   one'", function(){
+			assert.notStrictEqual(myApp.words("one there was was   one"), { one: 2, there: 1, was: 2 });
+		});
+	})
+})
+
