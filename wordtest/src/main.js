57359e8c6c1a7a298e1961cddfc8619427558d12
ObieWalker <obinnawalker@gmail.com>
2017-11-20 19:22:47 +0100

    add one more test

--- wordtest/src/main.js
+++ wordtest/src/main.js
@@ -4,26 +4,26 @@ module.exports = {
 
 words : (arg) => {
 
-  if(typeof arg === 'string' && arg !== 'null'){
+  if(typeof arg === 'string' && arg !== 'null' && arg!="" ){
     arg = arg.toLowerCase();
-    arg = arg.replace(/\s{2,}/g, ' ')//removes unneccessary white space
-    arg = arg.split(" ")//splits sentence into an array
+    arg = arg.replace(/\s{2,}/g, ' ');//removes unneccessary white space
+    arg = arg.split(" ");//splits sentence into an array
       let obj = {}
       arg.forEach(function(key){
          
         if (!(key in obj)){
-           var value = 0
-           obj[key]=value+1
+           var value = 0;
+           obj[key]=value+1;
           
         }else{
-          obj[key]=parseInt(obj[key])+1
+          obj[key]=parseInt(obj[key])+1;
         }
-      })
+      });
       
-      return obj
+      return obj;
       }
       else{
-        return 'please input a string';
+      return 'please input a string';
     }
-  }
+ }
 }
