900d21d9c1d7a436b229b517e684082d3e961f7b
ObieWalker <obinnawalker@gmail.com>
2017-11-22 08:55:23 +0100

    make changes tp adhere to airbnb style

--- wordtest/src/main.js
+++ wordtest/src/main.js
@@ -7,21 +7,18 @@
     arg = arg.toLowerCase();
     arg = arg.replace(/\s{2,}/g, ' ');//removes unneccessary white space
     arg = arg.split(" ");//splits sentence into an array
-      let obj = {}
-      arg.forEach(function(key){
-         
-        if (!(key in obj)){
-           var value = 0;
-           obj[key]=value+1; 
-        }else{
-          obj[key]=parseInt(obj[key])+1;
-        }
-      });
-      
-      return obj;
+    let obj = {}
+    arg.forEach(function(key){ 
+      if (!(key in obj)){
+        var value = 0;
+        obj[key]=value+1; 
+      }else{
+        obj[key]=parseInt(obj[key])+1;
       }
-      else{
-      return 'please input a string';
-    }
+    });  
+    return obj;
+  }else{
+    return 'please input a string';
+  }
  }
 export default words;
